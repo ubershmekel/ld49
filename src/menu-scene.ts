35af4bf62ad32df4b87dd495a5d8588ef2beade6
Yuval Greenfield <ubershmekel@gmail.com>
2021-10-01 22:27:54 -0700

    Calculating height kind of works

--- src/menu-scene.ts
+++ src/menu-scene.ts
@@ -1,10 +1,10 @@
 import 'phaser';
 import { gameHeight, gameWidth } from './config';
-import { catasesheetjsonUrl, fkey, preloadAll } from './assets-generated';
+import { fkey, preloadAll } from './assets-generated';
 
 export class MenuScene extends Phaser.Scene {
   private startKey!: Phaser.Input.Keyboard.Key;
-  private sprites: { s: Phaser.GameObjects.Image, r: number }[] = [];
+  private scoreText!: Phaser.GameObjects.Text;
 
   constructor() {
     super({
@@ -32,17 +32,17 @@ export class MenuScene extends Phaser.Scene {
     // this.add.sprite(gameWidth / 2, gameHeight / 2, 'ase')
     //   .play({ key: 'Idle', repeat: -1 }).setScale(6);
 
-    const text = this.add.text(0, 0, 'Press S to restart scene', {
+    this.scoreText = this.add.text(0, 0, 'Press S to restart scene', {
       fontFamily: "Helvetica",
     });
-    text.setFontSize(gameWidth * 0.07);
+    this.scoreText.setFontSize(gameWidth * 0.07);
 
     this.matter.world.setBounds();
-    const body1 = this.matter.add.rectangle(0.5 * gameWidth, gameHeight, gameWidth, gameHeight / 10, { isStatic: true });
+    this.matter.add.rectangle(0.5 * gameWidth, gameHeight, gameWidth, gameHeight / 10, { isStatic: true });
     this.matter.add.rectangle(0.1 * gameWidth, 0.1 * gameHeight, 0.1 * gameWidth, gameHeight / 10);
     this.matter.add.rectangle(0.2 * gameWidth, 0.1 * gameHeight, 0.2 * gameWidth, gameHeight / 10);
     this.matter.add.rectangle(0.3 * gameWidth, 0.1 * gameHeight, 0.3 * gameWidth, gameHeight / 10);
-    const body2 = this.matter.add.circle(0.5 * gameWidth, 0.1 * gameHeight, 16);
+    this.matter.add.circle(0.5 * gameWidth, 0.1 * gameHeight, 16);
     const star = '50 0 63 38 100 38 69 59 82 100 50 75 18 100 31 59 0 38 37 38';
     this.matter.add.fromVertices(0.3 * gameWidth, 0.3 * gameHeight, star, { restitution: 0.5 }, true);
 
@@ -66,15 +66,17 @@ export class MenuScene extends Phaser.Scene {
       this.scene.start(this);
     }
 
-    for (let i = 0; i < this.sprites.length; i++) {
-      const sprite = this.sprites[i].s;
-
-      sprite.y -= this.sprites[i].r;
-
-      if (sprite.y < -256) {
-        sprite.y = gameHeight;
+    let height = 0;
+    const maxHeight = 1000;
+    for (let i = 0; i < maxHeight; i++) {
+      const y = gameHeight - i * 100;
+      const bodies = this.matter.intersectRay(0, y, gameWidth, y);
+      if (bodies.length === 0) {
+        height = i;
+        break;
       }
     }
 
+    this.scoreText.text = `Height: ${height}`;
   }
 }
